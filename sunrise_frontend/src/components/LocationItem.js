c80dbaf897b3e647d683a732060fda38ecb9ceb0
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-06-09 12:59:47 -0600

    Backend added

--- sunrise_frontend/src/components/LocationItem.js
+++ sunrise_frontend/src/components/LocationItem.js
@@ -1,12 +1,24 @@
-import React from "react"
+import ReactDOM from "react-dom";
+import React, { useState } from "react"
+
+
 
 export default function LocationItem ({name, image, sunrise, lat, lng}) {
+
+    const [isFlipped, setIsFlipped] = useState(false);
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    }
+
     return(
-        <div>
-            <li className="location-item">
-                <h3>{name}</h3>
-                <img className="location-image" src={image} alt=""/>
-            </li>
-        </div>
+        <ReactCardFlip isFlipped={this.state.isFlipped} fliDirection="vertical">
+            <div>
+                <li className="location-item">
+                    <h3>{name}</h3>
+                    <img className="location-image" src={image} alt=""/>
+                </li>
+             </div>
+
+        </ReactCardFlip>
     )
 }
